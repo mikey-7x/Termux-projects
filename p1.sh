519d58e44d5710697f22aca3d8b7a57eb77b36bb
chauhan yogeshkumar r. <201270060+mikey-7x@users.noreply.github.com>
2025-04-07 23:36:39 +0530

    Update p1.sh

--- p1.sh
+++ p1.sh
@@ -12,7 +12,7 @@ termux-x11 :0 &
 # Wait for X11 to be ready
 sleep 3
 
-# Start U-Droid with XFCE4 and execute commands inside it
+# Start your linux (in my case it is U-Droid's ubuntu) with XFCE4 and execute commands inside it
 udroid login jammy:xfce4 << 'EOF'
 export DISPLAY=:0
 export PULSE_SERVER=tcp:127.0.0.1:4713
