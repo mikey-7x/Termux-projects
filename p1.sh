32e9903f1017119cd31c52556552e02b6e12587e
chauhan yogeshkumar r. <201270060+mikey-7x@users.noreply.github.com>
2025-04-08 10:56:12 +0530

    Update p1.sh

--- p1.sh
+++ p1.sh
@@ -1,10 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Set Display
-export DISPLAY=:0
-
 # Start PulseAudio
 pulseaudio --start --exit-idle-time=-1
+
+# Set Display
+export DISPLAY=:0
 
 # Start Termux-X11
 termux-x11 :0 &
@@ -14,7 +14,6 @@ sleep 3
 
 # Start your linux (in my case it is U-Droid's ubuntu) with XFCE4 and execute commands inside it
 udroid login jammy:xfce4 << 'EOF'
-export DISPLAY=:0
-export PULSE_SERVER=tcp:127.0.0.1:4713
-dbus-launch --exit-with-session startxfce4
+su - yogesh -c 'export PULSE_SERVER=tcp:127.0.0.1:4713; export DISPLAY=:0; dbus-launch --exit-with-session startxfce4'
 EOF
+
